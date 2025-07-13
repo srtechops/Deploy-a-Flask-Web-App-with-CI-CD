6871357f291a253f93d212e00f9be626109e20e0
shammugaraja M <shammugarajaofficial@gmail.com>
2025-07-13 18:45:48 +0530

    updated docker file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,6 @@ def srtechopsinfo():
 def srtechopsmobilenumber():
     return "FOR TRAINING ENQUIRY: +91 9150477118"
 
-app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
