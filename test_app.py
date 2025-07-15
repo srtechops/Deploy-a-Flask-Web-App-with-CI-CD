5f2f32ab89596d4eed9d0d1c7a91fb543b834050
shammugaraja M <shammugarajaofficial@gmail.com>
2025-07-15 20:36:26 +0530

    v4

--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
 from app import srtechopsmobilenumber
 
 def test_srtechopsmobilenumber():
-    assert srtechopsmobilenumber() == "FOR TRAINING ENQUIRY: +91 9150477118"
+    assert srtechopsmobilenumber() == '<h1 style="color:red;">FOR TRAINING ENQUIRY: +91 9150477118</h1>'
