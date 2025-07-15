4ee39a12f8bc3d47695b682e0bf142101cbb137c
shammugaraja M <shammugarajaofficial@gmail.com>
2025-07-15 20:52:44 +0530

    v7

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/info")
 def srtechopsinfo():
-    return '<h1 style="color:blue;">Welcome to SRTechOps</h1>'
+    return '<h1 style="color:Green;">Welcome to SRTechOps DevOps Class</h1>'
 
 @app.route("/contact")
 def srtechopsmobilenumber():
